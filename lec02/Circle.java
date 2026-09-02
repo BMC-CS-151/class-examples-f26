fa8afe23b3df4fc049d37016a72962f0dabe49aa
elizabethdinella <elizabeth.a.dinella@gmail.com>
2026-09-02 18:19:43 -0400

    static

---
+++ lec02/Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    static double PI = 3.14159;   // same value for every circle
+    double radius;                // each circle has its own
+
+    double area() {
+        return PI * radius * radius;
+    }
+
+    public static void main(String[] args) {
+	
+    }
+}
